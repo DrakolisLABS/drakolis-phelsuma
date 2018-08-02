bdda753e04816d64008fc0d9788da21987be0dfb
Mika Zimina <mika.zimina@iqoption.com>
2018-08-02 17:57:14 +0300

    added electron app

---
+++ src/electron.js
@@ -0,0 +1,18 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let file = `file://${process.cwd()}/dist/index.html`
+let url = 'http://localhost:8080/'
+
+let window = null
+
+app.on('ready', () => {
+  window = new BrowserWindow({width: 800, height: 600})
+  console.log(process.env.NODE_ENV)
+  if (process.env.NODE_ENV === 'PROD') {
+    window.loadURL(file)
+  } else {
+    window.loadURL(url)
+  }
+})
