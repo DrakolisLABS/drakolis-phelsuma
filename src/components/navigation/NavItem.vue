f8a41ac367cf86865409099eeff5fdd8b9ab4f2c
Drakon Lisowski <dragon.fox.66@yandex.ru>
2018-08-07 09:20:04 +0300

    Navigation Item Added

---
+++ src/components/navigation/NavItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-icon class='mr-1'>{{icon}}</v-icon>{{data.name}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  data: () => ({
+  }),
+  computed: {
+    icon: function () {
+      if(this.data.isSupported) {
+        return 'photo'
+      }
+      if(this.data.isFolder) {
+        if(this.data.isOpen) {
+          return 'folder-open'
+        }
+        return 'folder'
+      }
+    }
+  },
+  props: {
+    data: Object
+  }
+}
+</script>
